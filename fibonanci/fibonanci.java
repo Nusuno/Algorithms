384aa30e745a70b1d0e4da8d7d10f7e67e48acee
Nusuno <nuskung2547@gmail.com>
2024-12-04 11:03:52 +0700

    Fibonanci

--- fibonanci/fibonanci.java
+++ fibonanci/fibonanci.java
@@ -1,5 +1,29 @@
 package fibonanci;
 
+import java.util.Scanner;
+
 class Fibonanci {
-    
+    public static void main(String[] args) {
+        Scanner scanIn = new Scanner(System.in);
+        System.out.print("Enter a Fibonanci number: ");
+        int n = scanIn.nextInt();
+        scanIn.close();
+
+        int f0 = 0;
+        int f1 = 1;
+
+        if(n >= 0) {
+            System.out.println("f0 = " + f0);
+        }
+        if(n >= 1) {
+            System.out.println("f1 = " + f1);
+        }
+        for(int i = 2; i <= n; i++) {
+            int fn = f0 + f1;
+            System.out.println("f" + i + " = " + fn);
+            f0 = f1;
+            f1 = fn;
+        }
+        System.out.println("Value of Fibonanci is " + f1);
+    }
 }
