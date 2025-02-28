0881351e4382c0c25d956c86dfb587533361ca5c
Nusuno <nuskung2547@gmail.com>
2025-02-28 09:04:37 +0700

    sorting

---
+++ Sorting/Heap.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Heap {
+    
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+}
---
+++ Sorting/SortingAlgorithms.java
@@ -0,0 +1,106 @@
+import java.util.Arrays;
+
+public class SortingAlgorithms {
+    
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    
+    // Heap Sort
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+    
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+    
+    // Main method to test sorting algorithms
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        
+        // Bubble Sort
+        int[] bubbleArr = arr.clone();
+        bubbleSort(bubbleArr);
+        System.out.println("Bubble Sort: " + Arrays.toString(bubbleArr));
+        
+        // Selection Sort
+        int[] selectionArr = arr.clone();
+        selectionSort(selectionArr);
+        System.out.println("Selection Sort: " + Arrays.toString(selectionArr));
+        
+        // Insertion Sort
+        int[] insertionArr = arr.clone();
+        insertionSort(insertionArr);
+        System.out.println("Insertion Sort: " + Arrays.toString(insertionArr));
+        
+        // Heap Sort
+        int[] heapArr = arr.clone();
+        heapSort(heapArr);
+        System.out.println("Heap Sort: " + Arrays.toString(heapArr));
+    }
+}
